2325606cd0730427c343fdf5b2d177fa8fa40ed9
l0nkDev <43931415+l0nkDev@users.noreply.github.com>
2025-03-20 02:35:52 -0400

    tts funcional

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
 import 'dart:convert';
+import 'dart:io' show Platform;
 import 'dart:math';
 
+import 'package:flutter/foundation.dart' show kIsWeb;
 import 'package:flutter/material.dart';
 import 'package:flutter_chat_types/flutter_chat_types.dart' as types;
 import 'package:flutter_chat_ui/flutter_chat_ui.dart';
-import 'package:dart_openai/dart_openai.dart';
+import 'package:http/http.dart' as http;
+import 'package:flutter_tts/flutter_tts.dart';
 
 // For the testing purposes, you should probably use https://pub.dev/packages/uuid.
 String randomString() {
@@ -13,10 +16,8 @@ String randomString() {
   return base64UrlEncode(values);
 }
 
-void main() {
-  OpenAI.apiKey = "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";
-  OpenAI.requestsTimeOut = Duration(seconds: 120);
-  OpenAI.baseUrl = "https://openrouter.ai/api";
+
+void main() async{
   runApp(const MyApp());
 }
 
@@ -29,6 +30,8 @@ class MyApp extends StatelessWidget {
       );
 }
 
+enum TtsState { playing, stopped, paused, continued }
+
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key});
 
@@ -40,6 +43,128 @@ class _MyHomePageState extends State<MyHomePage> {
   final List<types.Message> _messages = [];
   final _user = const types.User(id: '82091008-a484-4a89-ae75-a22bf8d6f3ac');
   final _ai = const types.User(id: '82091008-a586-4a89-ae75-a22bf8d6f3ac');
+  late FlutterTts flutterTts;
+  String? language;
+  String? engine;
+  double volume = 0.5;
+  double pitch = 1.0;
+  double rate = 0.5;
+  bool isCurrentLanguageInstalled = false;
+
+  String? _newVoiceText;
+  int? _inputLength;
+
+  TtsState ttsState = TtsState.stopped;
+
+  bool get isPlaying => ttsState == TtsState.playing;
+  bool get isStopped => ttsState == TtsState.stopped;
+  bool get isPaused => ttsState == TtsState.paused;
+  bool get isContinued => ttsState == TtsState.continued;
+
+  bool get isIOS => !kIsWeb && Platform.isIOS;
+  bool get isAndroid => !kIsWeb && Platform.isAndroid;
+  bool get isWindows => !kIsWeb && Platform.isWindows;
+  bool get isWeb => kIsWeb;
+
+  @override
+  initState() {
+    super.initState();
+    initTts();
+  }
+
+  dynamic initTts() {
+    flutterTts = FlutterTts();
+
+    _setAwaitOptions();
+
+    if (isAndroid) {
+      _getDefaultEngine();
+      _getDefaultVoice();
+    }
+
+    flutterTts.setStartHandler(() {
+      setState(() {
+        print("Playing");
+        ttsState = TtsState.playing;
+      });
+    });
+
+    flutterTts.setCompletionHandler(() {
+      setState(() {
+        print("Complete");
+        ttsState = TtsState.stopped;
+      });
+    });
+
+    flutterTts.setCancelHandler(() {
+      setState(() {
+        print("Cancel");
+        ttsState = TtsState.stopped;
+      });
+    });
+
+    flutterTts.setPauseHandler(() {
+      setState(() {
+        print("Paused");
+        ttsState = TtsState.paused;
+      });
+    });
+
+    flutterTts.setContinueHandler(() {
+      setState(() {
+        print("Continued");
+        ttsState = TtsState.continued;
+      });
+    });
+
+    flutterTts.setErrorHandler((msg) {
+      setState(() {
+        print("error: $msg");
+        ttsState = TtsState.stopped;
+      });
+    });
+  }
+
+  Future<dynamic> _getLanguages() async => await flutterTts.getLanguages;
+
+  Future<dynamic> _getEngines() async => await flutterTts.getEngines;
+
+  Future<void> _getDefaultEngine() async {
+    var engine = await flutterTts.getDefaultEngine;
+    if (engine != null) {
+      print(engine);
+    }
+  }
+
+  Future<void> _getDefaultVoice() async {
+    var voice = await flutterTts.getDefaultVoice;
+    if (voice != null) {
+      print(voice);
+    }
+  }
+
+  Future<void> _speak() async {
+    await flutterTts.setVolume(volume);
+    await flutterTts.setSpeechRate(rate);
+    await flutterTts.setPitch(pitch);
+
+    if (_newVoiceText != null) {
+      if (_newVoiceText!.isNotEmpty) {
+        await flutterTts.speak(_newVoiceText!);
+      }
+    }
+  }
+
+  Future<void> _setAwaitOptions() async {
+    await flutterTts.awaitSpeakCompletion(true);
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    flutterTts.stop();
+  }
+
 
   @override
   Widget build(BuildContext context) => Scaffold(
@@ -56,6 +181,17 @@ class _MyHomePageState extends State<MyHomePage> {
     });
   }
 
+  void _addAnswer(String answer) {
+    _addMessage(
+      types.TextMessage(
+        author: _ai,
+        createdAt: DateTime.now().millisecondsSinceEpoch,
+        id: randomString(),
+        text: answer,
+      )
+    );
+  }
+
   void _handleSendPressed(types.PartialText message) {
     final textMessage = types.TextMessage(
       author: _user,
@@ -70,50 +206,31 @@ class _MyHomePageState extends State<MyHomePage> {
 
   Future<void> makeRequest(String text) async {
     print("sending '${text}' as request...");
-    final SystemMessage = OpenAIChatCompletionChoiceMessageModel(
-      content: [
-        OpenAIChatCompletionChoiceMessageContentItemModel.text(
-          "Answer in Spanish.",
-        ),
-      ],
-      role: OpenAIChatMessageRole.assistant,
-    );
-    final userMessage = OpenAIChatCompletionChoiceMessageModel(
-      content: [
-        OpenAIChatCompletionChoiceMessageContentItemModel.text(
-          text,
-        ),
-      ],
-      role: OpenAIChatMessageRole.user,
-    );
-    final chatStream = OpenAI.instance.chat.createStream(
-      model: "deepseek/deepseek-r1-zero:free",
-      messages: [
-        SystemMessage,
-        userMessage,
-      ],
-    );
     print("receiving response");
-    String response = "";
-    chatStream.listen(
-      (streamChatCompletion) {
-        final content = streamChatCompletion.choices.first.delta.content;
-        final String tmp =(content.toString());
-        response = response + tmp.substring(69, tmp.length-2);
-        print(response);
-      },
-      onDone: () {
-        print("done.");
-        print(_messages);
-        _addMessage(
-          types.TextMessage(
-            author: _ai,
-            createdAt: DateTime.now().millisecondsSinceEpoch,
-            id: randomString(),
-            text: response.substring(7, response.length-1),
-          )
-        );
+    var response = await http.post(Uri.http('192.168.0.18:11434','api/chat'), 
+    body: 
+    '''
+      {
+        "model": "deepseek-r1:1.5b",
+        "messages": [
+          {
+            "role": "system",
+            "content": "Answer in Spanish"
+          },
+          {
+            "role": "user",
+            "content": "$text"
+          }
+        ],
+        "stream": false
       }
+    '''
     );
+    var decodedResponse = jsonDecode(utf8.decode(response.bodyBytes)) as Map;
+    String res = decodedResponse['message']['content'].split('</think>')[1].substring(2);
+    language = "es_ES";
+    _addAnswer(res);
+    _newVoiceText = res;
+    _speak();
   }
 }
